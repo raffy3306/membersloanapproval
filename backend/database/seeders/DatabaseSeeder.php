a0c38aa3a99d923338a5848028a871e4e9cbe66e
raffy3306 <raffy3306@github.com>
2026-07-24 15:37:25 +0800

    Add members SQL data seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->call(MembersTableSeeder::class);
+
         $mainBranch = Branch::updateOrCreate(
             ['branch_code' => 'MAIN'],
             [
